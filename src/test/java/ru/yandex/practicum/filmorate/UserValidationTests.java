491fdf7dba4ace03e6511a34fd69d55efa745a7a
mkultin <106582988+mkultin@users.noreply.github.com>
2023-03-31 16:48:25 +0300

    Update UserValidationTests.java
    CodeStyle

--- src/test/java/ru/yandex/practicum/filmorate/UserValidationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserValidationTests.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.time.LocalDate;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 

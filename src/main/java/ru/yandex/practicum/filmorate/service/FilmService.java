4236428dffbd1141f3a32e942c7d4240d56b9f76
Elena <Alcabarena2006@ya.ru>
2023-04-15 21:27:23 +0300

    исправила фильтрацию фильмов

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,7 +8,6 @@ import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.director.DirectorDao;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.genre.GenreDao;
 import ru.yandex.practicum.filmorate.storage.like.LikeDao;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -22,16 +21,14 @@ public class FilmService {
     private final UserStorage userStorage;
     private final LikeDao likeDao;
     private final DirectorDao directorDao;
-    private final GenreDao genreDao;
 
 
     @Autowired
-    public FilmService(@Qualifier("filmBdStorage") FilmStorage filmStorage, UserStorage userStorage, LikeDao likeDao, DirectorDao directorDao, GenreDao genreDao) {
+    public FilmService(@Qualifier("filmBdStorage") FilmStorage filmStorage, UserStorage userStorage, LikeDao likeDao, DirectorDao directorDao) {
         this.filmStorage = filmStorage;
         this.userStorage = userStorage;
         this.likeDao = likeDao;
         this.directorDao = directorDao;
-        this.genreDao = genreDao;
     }
 
     public List<Film> getFilms() {
@@ -75,15 +72,6 @@ public class FilmService {
     }
 
     public List<Film> getPopularFilm(int count, Integer genreId, Integer year) {
-        // проверим валидность присланного жанра
-        if (genreId != null) {
-            genreDao.findById(genreId);
-        } else
-            throw new NotFoundException("Попробуйте задать другой жанр для фильтрации популярных фильмов");
-        // проверим валидность присланного года
-        if (year != null && year < 0) {
-            throw new NotFoundException("Год не может быть отрицательным");
-        }
         return filmStorage.getPopularFilm(count, genreId, year);
     }
 

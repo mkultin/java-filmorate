e196da6febcb37e98ee779cdcccd3a3c1bf2e6a0
Marina <copperraspberry@yandex.ru>
2023-04-15 11:24:42 +0300

    feat: добавить рекомендации

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -95,7 +95,7 @@ public class UserService {
         }
     }
 
-    public Set<Film> getRecommendedFilms(Long userId){
+    public Set<Film> getRecommendedFilms(Long userId) {
         User user = userBdStorage.getUserById(userId); //проверка существования пользователя
         return likeDao.getRecommendedFilms(userId);
     }

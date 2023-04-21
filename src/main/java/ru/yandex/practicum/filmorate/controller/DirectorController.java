d34d4e92c75bb7d43d81e94df33b71b59965931f
Elena <87905994+Elena315@users.noreply.github.com>
2023-04-21 22:13:28 +0300

    Add common films (#25)
    * Добавлены логи

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.service.DirectorService;
@@ -8,35 +9,41 @@ import ru.yandex.practicum.filmorate.service.DirectorService;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
-@RequestMapping("/directors")
 @RequiredArgsConstructor
+@RequestMapping("/directors")
 public class DirectorController {
 
     private final DirectorService directorService;
 
     @GetMapping
     public List<Director> getDirectors() {
+        log.info("GET /directors : get list of all directors");
         return directorService.getDirectors();
     }
 
     @GetMapping("/{id}")
     public Director getDirectorById(@PathVariable Integer id) {
+        log.debug("GET /directors/{} : get director by ID", id);
         return directorService.getDirectorById(id);
     }
 
     @PostMapping
     public Director addDirector(@Valid @RequestBody Director director) {
+        log.debug("POST /directors : add Director - {}", director);
         return directorService.addDirector(director);
     }
 
     @PutMapping
     public Director updateDirector(@Valid @RequestBody Director director) {
+        log.debug("PUT /directors : update director - {}", director);
         return directorService.updateDirector(director);
     }
 
     @DeleteMapping("/{id}")
     public void deleteDirector(@PathVariable Integer id) {
+        log.debug("DELETE /directors/{} : delete director by ID.", id);
         directorService.deleteDirector(id);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.service.ReviewService;
@@ -8,6 +9,7 @@ import ru.yandex.practicum.filmorate.service.ReviewService;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/reviews")
 @RequiredArgsConstructor
@@ -17,27 +19,32 @@ public class ReviewController {
 
     @PostMapping
     Review addReview(@RequestBody @Valid Review review) {
+        log.info("POST /reviews : create review - {}", review);
         return reviewService.create(review);
     }
 
     @PutMapping
     Review updateReview(@RequestBody @Valid Review review) {
+        log.info("PUT /reviews/ : update review - {}", review);
         return reviewService.update(review);
     }
 
     @DeleteMapping("/{id}")
     void deleteReview(@PathVariable Long id) {
+        log.info("DELETE /reviews/{} : delete review by ID", id);
         reviewService.delete(id);
     }
 
     @GetMapping("/{id}")
     Review getReviewById(@PathVariable Long id) {
+        log.info("GET /reviews/{} : get review by ID", id);
         return reviewService.findById(id);
     }
 
     @GetMapping
     List<Review> getReviews(@RequestParam(value = "filmId", defaultValue = "0") Long filmId,
                             @RequestParam(value = "count", defaultValue = "10") int count) {
+        log.info("GET /reviews?filmId={}&count={} : get list of reviews", filmId, count);
         return reviewService.getReviews(filmId, count);
     }
 
@@ -49,19 +56,22 @@ public class ReviewController {
 
     @PutMapping("/{id}/dislike/{userId}")
     void addDislikeReview(@PathVariable Long id,
-                       @PathVariable Long userId) {
+                          @PathVariable Long userId) {
+        log.info("PUT /reviews/{}/dislike/{} : add dislike to review from user", id, userId);
         reviewService.addDislike(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     void deleteLikeReview(@PathVariable Long id,
-                       @PathVariable Long userId) {
+                          @PathVariable Long userId) {
+        log.info("DELETE /reviews/{}/like/{} : remove like for review from user", id, userId);
         reviewService.deleteLike(id, userId);
     }
 
     @DeleteMapping("/{id}/dislike/{userId}")
     void deleteDislikeReview(@PathVariable Long id,
-                          @PathVariable Long userId) {
+                             @PathVariable Long userId) {
+        log.info("DELETE /reviews/{}/dislike/{} : remove dislike for review from user", id, userId);
         reviewService.deleteLike(id, userId);
     }
 }

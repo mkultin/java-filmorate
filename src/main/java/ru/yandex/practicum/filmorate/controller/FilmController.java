0f389af85d4615e2d64cbc39b8ad9c32fdc0d772
Elena <Alcabarena2006@ya.ru>
2023-04-21 20:09:42 +0300

    Добавлены логи

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -80,7 +80,7 @@ public class FilmController {
 
     @GetMapping("/common")
     public List<Film> getCommonFilms(@RequestParam Long userId, @RequestParam Long friendId) {
-        log.info("getCommonFilms (GET /common?userId={}&friendId={}):",userId, friendId);
+        log.info("getCommonFilms (GET /common?userId={}&friendId={}):", userId, friendId);
         return filmService.getCommonFilms(userId, friendId);
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,13 +3,14 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.event.Event;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.event.Event;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.validation.Valid;
-import java.util.*;
+import java.util.List;
+import java.util.Set;
 
 @Slf4j
 @RestController
